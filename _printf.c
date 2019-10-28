649f27420dd395470d9d9484e8289c0c65658b2d
Diego <dfbq91@gmail.com>
2019-10-28 01:54:56 +0000

    printf with %c, %s, '%' and NULL cases

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _printf - prints anything (output) according to a format
+ * @format: character string (c, s, %)
+ * Return: numbers of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	const char *c, *s; /*To be initialized as char format and string format*/
+
+	va_start(list, format);
+	while (format != NULL)
+	{
+		for (c = format; *c != '\0'; c++)
+		{
+			if (*c != '%')
+			{
+				putchar(*c);
+				continue;
+			}
+
+			switch (*++c)
+			{
+			case 's':
+				for (s = va_arg(list, char*); *s != '\0'; s++)
+					putchar(*s);
+				break;
+			case 'c':
+				putchar(va_arg(list, int));
+				break;
+			case '%':
+				putchar('%');
+			}
+		}
+		break;
+	}
+	va_end(list);
+	return (0);
+}
---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+#endif
