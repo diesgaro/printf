e1526cebb3e588cfd9d39b0490fcf4b5e5c78792
Diego Garzon <1265@holbertonschool.com>
2019-10-28 23:59:45 +0000

    Fix DG

--- _printf.c
+++ _printf.c
@@ -8,51 +8,91 @@
 int _printf(const char *format, ...)
 {
 	va_list list;
-	const char *c, *s, *index; /*To be initialized as char format and string format*/
+	int i = 0, index = 0, x = 0;
+
+	op_t options[] = {
+		{"c", op_char},
+		{"s", op_string},
+		{"%", op_percent},
+		{NULL, NULL}
+	};
 
 	va_start(list, format);
 
 	if (format == NULL)
-		return (-1);
-	for (c = format; *c != '\0'; c++)
 	{
-		if (*c != '%')
+		return (-1);
+	}
+	else
+	{
+		while (format[i] != '\0')
 		{
-			_putchar(*c);
-			continue;
-		}
-		else
-		{
-			index = c + 1;
-
-			switch (*index)
+			if (format[i] != '%')
 			{
-			case 's':
-				s = va_arg(list, char*);
-				if (s == NULL)
-					s = "(null)";
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-				}
-				c = index;
-				break;
-			case 'c':
-				_putchar(va_arg(list, int));
-				c = index;
-				break;
-			case '%':
-				_putchar(*c);
-				c = index;
-				break;
-			default:
-				if (*index != '\0')
-					_putchar(*c);
-				break;
+				_putchar(format[i]);
 			}
+			else
+			{
+				index = i + 1;
+				x = 0;
+				while (options[x].op != '\0')
+				{
+					if (format[index] == options[x].op[0])
+						options[x].f(list);
+					x++;
+				}
+				i = index;
+			}
+			i++;
 		}
 	}
 	va_end(list);
 	return (0);
+}
+
+/**
+ * op_char - Function that prints char
+ *
+ * @list: Variable type va_list
+ *
+ *Return: 0
+ *
+ */
+void op_char(va_list list)
+{
+	_putchar(va_arg(list, int));
+}
+
+/**
+ * op_string - Function that prints string
+ *
+ * @list: Variable type va_list
+ *
+ *Return: 0
+ *
+ */
+void op_string(va_list list)
+{
+	char *s = va_arg(list, char*);
+
+	if (s == NULL)
+		s = "(null)";
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
+
+/**
+ * op_percent - Function that prints percent
+ *
+ * @list: Variable type va_list
+ *
+ *Return: 0
+ *
+ */
+void op_percent()
+{
+	_putchar('%');
 }
--- holberton.h
+++ holberton.h
@@ -1,9 +1,27 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-#endif
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The format
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)();
+} op_t;
+
+void op_char(va_list list);
+void op_string(va_list list);
+void op_percent();
+
+#endif /* HOLBERTON_H */
